924520058c5fbfb86c0423e7651a054f29ae7f0d
levyrodrigues23 <joselevyrodrigues23@gmail.com>
2025-11-13 20:37:35 -0300

    feat: algumas alterações feitas nos métodos defeituososo da classe Cursos.py

---
+++ poo/atv_campus_ufc/README.md
@@ -0,0 +1,7 @@
+### explicando um pouco diretamente sobre como realizei a atividade do campus ufc
+
+> - olhando um pouco afundo sobre a atividade, pesquisei diretamente os locais onde abrangiam os campus da ufc pela regiao do Ceara, e após esse processo, conseguir realizar boa parte do código da seguinte maneira:
+
+> - Criei um arquivo chamado ```CampusLista.py``` com o objetivo de armazenar os campus de itapaje, e usei a lógica de criar um dicionário para poder mapear melhor em cima dos cursos que abrangiam o respectivo dicionário. Esse arquivo contém alguns métodos específicos que foram aplicados
+
+> - Há também um arquivo chamado ```Cursos.py``` que contém alguns métodos baseado em um Crud, ou seja, é possivel adicionar um curso ao mesmo tempo que é possivel remover, atualizar e verificar se um curso realmente existe. No final eu realizei alguns testes respectivos.
--- poo/atv_campus_ufc/cursos.py
+++ poo/atv_campus_ufc/cursos.py
@@ -3,20 +3,51 @@ class Curso:
         self.cursos = []
         
         
-    def adicionar_curso(self, curso):
-        self.cursos.append(curso)
-        print(f"o curso {curso} foi adicionado com sucesso!!!")
+    def adicionar_curso(self, curso: str):
+        print(f"o curso {curso} foi adicionado com sucesso.")
+        return self.cursos.append(curso)
 
-    def atualizar_curso(self, curso, nome_atualizado):
+        
+
+    def atualizar_curso(self, curso: str, nome_atualizado: str):
         if not curso in self.cursos:
             print("o curso ainda não foi cadastrado, então tecnicamente não tem como atualizar")
-        for curso in self.cursos:
-            curso = nome_atualizado
-            print(f"o curso foi atualizado e agora é assim: {curso}")
+        for indice, nome_curso in enumerate(self.cursos):
+            if nome_curso == curso:
+                self.cursos[indice] = nome_atualizado       
+                print(f"o curso {curso} foi atualizado para {nome_atualizado}")         
+                
+                
             
-    def remover_curso(self, curso):
+    def remover_curso(self, curso: str):
         if not curso in self.cursos:
             print("esse curso não foi cadastrado!")
-        for curso in self.cursos:
-            self.cursos.pop(curso)
-            print(f"o curso {curso} foi completamente removido.")
+        for i in self.cursos:
+            if i == curso:
+                print(f"o curso {curso} foi completamente removido.")
+                return self.cursos.remove(curso)
+                
+                
+            
+    def verificar_curso(self, curso):
+        print(self.cursos)
+        if not curso in self.cursos:
+            print("o curso que voce digitou não consta cadastramento.")
+        for i in self.cursos:
+            if i == curso:
+                print(f"o curso {curso} existe")
+                
+# vou testar aqui pra ver se realmente ta funcionando ou se eu fiz errado, senão eu vou ter que corrigir miseravelmente
+
+curso = Curso()
+
+
+curso.atualizar_curso("medicina", "medicina veterinaria")
+curso.remover_curso("medicina")
+curso.remover_curso("medicina veterinaria")
+curso.verificar_curso("medicina")
+curso.adicionar_curso("medicina")
+curso.verificar_curso("medicina veterinaria")
+curso.verificar_curso("medicina")
+
+# atualmente pelo que vejo está funcionando normalmente, isso é realmente bom
